ff90e5b083174763f6c6c825ea92aa088e1bcbf3
FaiqErlang <ghozyerlanggafaiq@gmail.com>
2024-10-24 01:14:58 +0700

    menginstall tailwind dan mengkonfigurasinya

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Navbar } from "./components/navbar";
+import { HeroSection } from "./components/heroSection";
+import "./styles/navbar.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Navbar />
+    <HeroSection />
   </StrictMode>
 );
