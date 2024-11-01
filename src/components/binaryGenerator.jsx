3216f551eeea52129eb264c69ada430153378bf3
FaiqErlang <ghozyerlanggafaiq@gmail.com>
2024-11-01 21:12:00 +0700

    making ui of heroSection

--- src/components/binaryGenerator.jsx
+++ src/components/binaryGenerator.jsx
@@ -3,7 +3,7 @@ export function BinaryGenerator() {
     return (N >>> 0).toString(2);
   }
 
-  let N = Math.floor(Math.random() * 100);
+  let N = Math.floor(Math.random() * 1000);
   let binary = decimalToBinary(N);
 
   return <>{binary}</>;
---
+++ src/components/editSection.jsx
@@ -0,0 +1,7 @@
+export function EditSection() {
+  return (
+    <>
+      <div className="py-4 bg-white rounded-full  row-auto text-black m-20"></div>
+    </>
+  );
+}
