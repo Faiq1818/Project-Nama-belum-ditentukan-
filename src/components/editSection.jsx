4050a1fd5aa67bf54d59935d67552f0c52b3d121
Faiq1818 <ghozyerlanggafaiq@gmail.com>
2025-01-12 14:39:56 +0700

    making a bar for changing mode

--- src/components/editSection.jsx
+++ src/components/editSection.jsx
@@ -1,7 +1,13 @@
 export function EditSection() {
   return (
     <>
-      <div className="py-4 bg-white rounded-full  row-auto text-black"></div>
+      <div className="mb-40s py-1 px-6 bg-white rounded-xl text-gray-900 flex flex-row gap-10">
+        <div>waktu</div>
+        <div>Easy</div>
+        <div>Medium</div>
+        <div>Hard</div>
+        <div>Very Hard</div>
+      </div>
     </>
   );
 }
--- src/components/heroSection.jsx
+++ src/components/heroSection.jsx
@@ -4,13 +4,16 @@ import { EditSection } from "./editSection";
 export function HeroSection() {
   return (
     <>
-      <div className=" items-center justify-center flex-1 flex-col flex">
+      <div
+        style={{ fontFamily: "flexiibm" }}
+        className=" items-center justify-center flex-1 flex-col flex gap-11"
+      >
         <EditSection />
         <div className="justify-center flex font-semibold  text-5xl">
           <BinaryGenerator />
         </div>
 
-        <button className="bg-white text-black rounded-full px-9 py-2">
+        <button className="bg-white text-gray-900 rounded-md px-9 py-9">
           halo
         </button>
       </div>
